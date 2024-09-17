81a4f482544a332b6e8fe0b9059974fef05a8a0f
Sowmyachowdary412 <sowmyachowdary2022@gmail.com>
2024-09-17 11:28:55 +0530

    main file

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,3 @@
 def hello_world():
     print("Hello world")
-hello_world()
+#hello_world()
---
+++ main.py
@@ -0,0 +1,6 @@
+import hello_world
+def main():
+    hello_world.hello_world()
+
+if __name__ == '__main__':
+    main()
